05a46985fb18f78e4e8cc6d583f3145d8a24493e
Godwin Okpe <okpegodwin18@yahoo.com>
2024-01-29 12:40:10 +0000

    Attempt at the utf validation task: checking whether a set of data input conforms to utf8 pattern

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""
+Module to find out the UTF-8 status of a set of data
+"""
+
+
+def validUTF8(data):
+    """
+    Function to determine the utf-8 status of input data
+    """
+    def is_valid_following_byte(byte):
+        """
+        Function to check whether the leading bits match
+        expected pattern.
+        """
+        return (byte & 0b11000000) == 0b10000000
+
+    i = 0
+    while i < len(data):
+        leading_ones = bin(data[i])[2:].rjust(8, '0').index('0')
+        if leading_ones == 0:
+            i += 1
+        elif 1 <= leading_ones <= 4:
+            for j in range(1, leading_ones):
+                if i + j >= len(data) or not \
+                        is_valid_following_byte(data[i + j]):
+                    return False
+            i += leading_ones
+        else:
+            return False
+
+    return True
---
+++ 0x04-utf8_validation/README.md
@@ -0,0 +1,5 @@
+# 0x04-utf8_validation
+
+This project determines if a set of data is valid UTF-8 or not.
+
+
