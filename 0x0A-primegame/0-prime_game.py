50a71fd9804ef038b9eae3beb0d90f46b879ecf4
Godwin Okpe <okpegodwin18@yahoo.com>
2024-03-14 18:15:30 +0000

    Reworked the game using a different approach

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -14,41 +14,32 @@ def is_prime(num):
     return True
 
 
-def next_prime(start):
-    """ Returns the next prime number from the current number """
-    while True:
-        if is_prime(start):
-            return start
-        start += 1
-
-
-def optimal_winner(n):
-    """
-    Returns the winner based on the n
-    """
-    current = 2
-    while n > 1:
-        current = next_prime(current)
-        if n % current == 0:
-            return "Ben"
-        n //= current
-    return "Maria"
-
-
 def isWinner(x, nums):
+    """
+    Functio to return the winner in the game depending on
+    who won the most round
+    """
     maria_wins = 0
     ben_wins = 0
 
-    for n in nums:
-        winner = optimal_winner(n)
-        if winner == "Maria":
-            maria_wins += 1
-        elif winner == "Ben":
+    for _ in range(x):
+        # Find the first prime number in the list
+        for num in nums:
+            if is_prime(num):
+                nums = [n for n in nums if n % num != 0]
+                break
+
+        # Check if either player can make a move
+        if not any(is_prime(num) for num in nums):
+            # Ben wins if Maria cannot make a move
             ben_wins += 1
+        else:
+            # Maria wins if Ben cannot make a move
+            maria_wins += 1
 
     if maria_wins > ben_wins:
         return "Maria"
-    elif maria_wins < ben_wins:
+    elif ben_wins > maria_wins:
         return "Ben"
     else:
         return None
